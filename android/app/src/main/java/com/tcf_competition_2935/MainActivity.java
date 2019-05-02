70c026829cb7778f8bb2ed941a4adf0269e82d9f
Crowdbotics <team@crowdbotics.com>
2019-05-02 15:36:41 +0000

    initial commit

---
+++ android/app/src/main/java/com/tcf_competition_2935/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tcf_competition_2935;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "tcf_competition_2935";
+    }
+}
